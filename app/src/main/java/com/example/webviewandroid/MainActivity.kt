ee100f9e29407b39010bd36a987d22160571c0e3
joseluisbn <joseluisbn@gmail.com>
2020-03-05 13:10:54 +0100

    WebView implementation

--- app/src/main/java/com/example/webviewandroid/MainActivity.kt
+++ app/src/main/java/com/example/webviewandroid/MainActivity.kt
@@ -2,11 +2,50 @@ package com.example.webviewandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // Declaring constant to define URL
+
+    private val BASE_URL = "https://baseavalancha.com"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Enabling JavaScript
+
+        val settings :WebSettings = WebView.settings
+        settings.javaScriptEnabled = true
+
+        // WebView Insertion
+
+        WebView.webViewClient = object : WebViewClient() {
+
+        }
+
+        WebView.webChromeClient = object  : WebChromeClient (){
+
+        }
+
+
+        //Loading URL to WebView
+
+        WebView.loadUrl(BASE_URL)
+
+        }
+
+        override fun onBackPressed(){
+            if(WebView.canGoBack()) {
+                WebView.goBack()
+            } else {
+                super.onBackPressed()
+            }
+
+        }
+
     }
-}
